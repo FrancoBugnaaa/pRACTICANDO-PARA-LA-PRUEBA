5030ca7e79959cf99354ed630b0d22d9b59ce85c
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-04-11 22:22:19 -0300

    aaaa
    aaaa

---
+++ Assets/Scenes/Scripts/Ej05.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej05 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    int resultado;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado = num1 + num2;
+        Debug.Log(num1 + " / " + num2 + " = " + resultado);
+        if (num2 => 0) 
+        
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
